041694b78dd08b9c8db5c2e176fc64e5f1dd94c7
“sayan <sayansenapati2544@gmail.com>
2025-08-10 20:29:09 +0530

    solving the theme error

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -2,7 +2,17 @@
 
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+type Theme = 'dark' | 'light' | 'system';
+type Attribute = 'class' | 'data-theme';
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: Attribute | Attribute[];
+  defaultTheme?: Theme;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
